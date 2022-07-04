23f0f744b837ca2fbf9112359bc8e44e966b1dc5
Colton Davenport <coltondavenport@FVFGW3VBQ6LR-Colton-Davenport.local>
2022-07-04 14:50:32 -0700

    added comments, renamed life.js to Main.js

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,11 +1,9 @@
 /**
- * Summary. (use period)
+ * Summary. A class that implements the Game of Life. Conway's game of life by default
  *
  * Description. (use period)
  *
  * @file   This files runs a game of life.
- * @author AuthorName.
- * @since  x.x.x
  */
 
 const DEFAULT_RULES  = {
@@ -14,21 +12,53 @@ const DEFAULT_RULES  = {
 }
 
 
+/**
+ * Summary. Implements the Game of Life
+ *
+ * Description. Accepts a board, then can handle the logic for evolving that
+ *              board according to the Game of Life rules. 
+ * 
+ *              By default, those rules are:
+ *                  1) Any dead cell adjacent to exactly 3 live cells becomes alive
+ *                  2) Any live cell adjacent to 2 or 3 live cells survives
+ *                  3) Every other live cell dies
+ * 
+ *              The values of those rules can be adjusted.
+ *
+ * @access     public
+ *
+ *
+ * @param {[[boolean]]}   board  The new board for the game of life
+ *
+ */
+
 class Game {
 
     #board
-    #boardHasChanged = false;
     #rules = {}
-
+    
     constructor(board, rules = DEFAULT_RULES){
 
         // test that the board is a 2d array of booleans 
-        this.setBoard(board)
+        this.setBoard(board);
         
         this.setRules(rules);
 
     }
 
+
+    /**
+     * Summary. Assign a board array
+     *
+     * Description. Sets a deep copy of the parameter as the board. Throws an exception
+     *              if the board isn't a 2d array of booleans
+     *
+     * @access     public
+     *
+     *
+     * @param {[[boolean]]}   board  The new board for the game of life
+     *
+     */
     setBoard(board){
 
         // check that the new board is a 2d array of booleans
@@ -45,8 +75,6 @@ class Game {
             throw(err)
         }
 
-        
-
         // deep copy the new board to the board property
 
         this.#board = [];
@@ -61,22 +89,50 @@ class Game {
         }
     }
 
-    #assignRuleValues(rulesProperty, values){
+    /**
+     * Summary. Returns the values from an array that are valid for a game of
+     *          life rules sets
+     *
+     * Description. Accepts an object with the "live" and "reproduce" properties
+     *                  to set the game of life's rules. Will throw exceptions if 
+     *                  the live or reproduce properties aren't set
+     *
+     * @access     private
+     *
+     *
+     * @param {object}   rules  The new rules for the game of life
+     *
+     */
+    #getValidRulesValues(values){
+
+        let validValues = [];
         for(const ruleValue of values){
             if(typeof ruleValue !== "number" && ruleValue !== Math.floor(ruleValue)){
-                throw("Rule values must be integers")
+                console.warn("Rule values must be integers, ", ruleValue, " ignored")
             }
-            if(ruleValue>8){
-                console.warn("A rule value over 8 will have no affect")
-            } 
-            else if(ruleValue < 0){
-                console.warn("A negative rule value will have no effect")
+            else if(ruleValue < 0 || ruleValue > 8){
+                console.warn("A rule value of ", ruleValue, " ignored");
             } 
             else {
-                rulesProperty.push(ruleValue);
+                validValues.push(ruleValue);
             }
         }
+        return validValues;
     }
+
+    /**
+     * Summary. set the rules for the game of life
+     *
+     * Description. Accepts an object with the "live" and "reproduce" properties
+     *                  to set the game of life's rules. Will throw exceptions if 
+     *                  the live or reproduce properties aren't set
+     *
+     * @access     public
+     *
+     *
+     * @param {object}   rules  The new rules for the game of life
+     *
+     */
     setRules(rules){
         if(rules === undefined){
             throw("Rules must be defined")
@@ -86,11 +142,11 @@ class Game {
             throw ('Rules must include a "live" and a "reproduce" property')
         }
 
-        this.#rules.live = [];
-        this.#assignRuleValues(this.#rules.live, rules.live)
+        this.#rules.live = this.#getValidRulesValues(rules.live)
+        this.#rules.reproduce = this.#getValidRulesValues(rules.reproduce)
 
-        this.#rules.reproduce = [];
-        this.#assignRuleValues(this.#rules.reproduce, rules.reproduce)
+        console.log(this.#rules)
+
     }
 
 
@@ -173,15 +229,20 @@ class Game {
         return true;
     }
     
-    hasChanged() {
-        return this.#boardHasChanged;
-    }
-
+    /**
+     *
+     * Summary. Advance the state of the game of life by 1 turn
+     *
+     * Description. Using the rules set for this game, the cells are updated. 
+     *              Cells will either die, reproduce, or stay the same.
+     *
+     * @access     public
+     *
+     */
     Evolve () {
         // create an array to store counts of each cell's neighbours
         let neighbourCounts =  this.#GetNeighbourCounts();
 
-        this.#boardHasChanged = false;
 
         // update the board
         this.#board.forEach((row, rowIndex) => {
@@ -189,18 +250,27 @@ class Game {
                 // if a live cell doesn't have the right number of neighbours, it dies
                 if(cell && !this.#rules.live.includes(neighbourCounts[rowIndex][colIndex])) {
                     this.#board[rowIndex][colIndex] = false;
-                    this.#boardHasChanged = true;
 
                 } else if(!cell && this.#rules.reproduce.includes(neighbourCounts[rowIndex][colIndex])){
-                    this.#board[rowIndex][colIndex] = true;
-                    this.#boardHasChanged = true;
+                    this.#board[rowIndex][colIndex] = true; 
                 }
             })
         });
     }
 
-    this
-
+    /**
+     *
+     * Summary. Returns the state of this game as html
+     *
+     * Description. Live cells are represented with "+", 
+     *              dead with "-", and rows are separated 
+     *              with "<br />""
+     *
+     * @access     public
+     *
+     * @return {String} A string representing the state of the board
+     *                      with some html tags
+     */
     toHTML(){
         let string = "";
         this.#board.forEach((row, index) => {
